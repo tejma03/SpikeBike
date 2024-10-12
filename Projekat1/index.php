8b3563e8eb16dbd148385be429c9fe7aaaef6efe
Tejmesss <166008724+tejma03@users.noreply.github.com>
2024-10-12 13:28:08 +0200

    Add files via upload
    Glavni deo

---
+++ Projekat1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Početna</title>
+    <meta name="description" content="SpikeBike biciklistička radnja."/>
+    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap">
+</head>
+<body>
+<div class="background-text">
+    <h1>Spike<span> Bike</span> - Za Bicikliste, od Biciklista</h1>
+    <p>Sa strašću prema biciklizmu, pružamo vrhunske usluge. Saznajte više o našoj priči i vrednostima koje nas pokreću.</p>
+</div>
+<header class="header">
+    <a href="#" class="name">Spike<span>Bike</span></a>
+    <nav class="navbar">
+        <a href="index.php">Početna</a>
+        <a href="about.html">O nama</a>
+        <a href="skills.html">Veštine</a>
+        <a href="services.html">Servisi</a>
+        <a href="contact.html">Kontakt</a>
+        <a href="biography.html">Biografija</a>
+        <a href="blog.html">Zadovoljne mušterije</a>
+        <a href="questions.html">Pitanja</a>
+    </nav>
+</header>
+<footer class="footer">
+    <p>&copy; 2024 SpikeBike. All rights reserved.   <?php echo date("d.m.Y"); ?></p>
+</footer>
+</body>
+</html>
